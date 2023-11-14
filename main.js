1bd9a95d32f1be0e0d738be36f26a03bc6a069e7
michalwitek <michal.witek@codilime.com>
2023-11-14 15:58:28 +0100

    add cube rotation

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module'
+import dat from 'dat.gui';
 
-// import dat from 'dat.gui';
-// import gsap from 'gsap';
+const world = {
+    box: {
+        width: 1,
+        height: 1,
+        depth: 1
+    }
+}
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
@@ -17,13 +24,13 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper( 10 );
-scene.add( axesHelper );
+const axesHelper = new THREE.AxesHelper(10);
+scene.add(axesHelper);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.BoxGeometry(world.box.width, world.box.height, world.box.depth);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 
 // renderer.render(scene, camera);
@@ -31,13 +38,28 @@ scene.add( cube );
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+const guiGenerateBox = () => {
+    cube.geometry.dispose();
+    cube.geometry = new THREE.BoxGeometry(world.box.width, world.box.height, world.box.depth);
+};
+
+const gui = new dat.GUI();
+gui.add(world.box, 'width', 1, 20).onChange(guiGenerateBox);
+gui.add(world.box, 'height', 1, 20).onChange(guiGenerateBox);
+gui.add(world.box, 'depth', 1, 20).onChange(guiGenerateBox);
+
 function animate() {
     requestAnimationFrame(animate)
     render()
 }
+
 function render() {
     stats.update();
     renderer.render(scene, camera)
+
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
+    cube.rotation.z += 0.005;
 }
 
 animate()
