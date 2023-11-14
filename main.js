c9ad76c5cd60a100699c3a1ede8b12d1f70f74a4
michalwitek <michal.witek@codilime.com>
2023-11-14 15:39:16 +0100

    add orbit controls, axesHelper, stats and cube

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 import './style.css'
-// import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module'
+
 // import dat from 'dat.gui';
 // import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 
+camera.position.set(1, 1, 10)
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper( 10 );
+scene.add( axesHelper );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+// renderer.render(scene, camera);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
-
 
 function animate() {
     requestAnimationFrame(animate)
     render()
 }
-
 function render() {
     stats.update();
     renderer.render(scene, camera)
